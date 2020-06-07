bc39b6211d3a0b2ac46ff8b6a38a16fbcf2994c1
Joshua Canlas <canlas.joshua0121@gmail.com>
2020-06-07 13:12:42 -0600

    updated 6/7/2020

--- Apps/Interactive_English_Dictionary/exercise.py
+++ Apps/Interactive_English_Dictionary/exercise.py
@@ -6,3 +6,16 @@ con = mysql.connector.connect(
     host="108.167.140.122",
     database="ardit700_pm1database"
 )
+
+cursor = con.cursor()
+
+word = input("Enter a word: ")
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % word)
+results = cursor.fetchall()
+
+if results:
+    for result in results:
+        print(result[1])
+else:
+    print("No word found")
