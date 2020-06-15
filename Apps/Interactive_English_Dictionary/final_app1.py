7a7f11615c1f525e106bf1094328a57e939ee6cb
Joshua Canlas <canlas.joshua0121@gmail.com>
2020-06-15 16:24:14 -0600

    Updated 6/15/2020

---
+++ Apps/Interactive_English_Dictionary/final_app1.py
@@ -0,0 +1,35 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, N if no: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+
+word = input("Enter word: ")
+
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
